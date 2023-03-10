63a4b133a15a4d991d330cd674033d51dab7adeb
Al Muhdil Karim <al.muhdil.0@gmail.com>
2023-03-10 17:47:47 +0700

    Add files via upload

---
+++ aviarium.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+Plugin Name: Aviarium
+Plugin URI: https://lectore.com
+Description: Development framework and administrator toolkit 
+Version: 0.1
+Author: Al Muhdil Karim
+Author URI: https://almuhdilkarim.com
+*/
+
+wp_includes();
+
+// require_once 'scheme/scheme-system-v5.php';
